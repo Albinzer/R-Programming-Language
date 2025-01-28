3402eff2be4fa61e8089dd3e6abbf5d3ccaf9735
Abu-Bakkar Siddik Zilhaj <160333632+Albinzer@users.noreply.github.com>
2025-01-28 12:30:32 +0600

    Create Barchart.r

---
+++ Barchart.r
@@ -0,0 +1,6 @@
+# Barchart
+h<- c (12,50,34,16,17)
+m<- c("Jan","Feb","Mar","Apr","May")
+
+barplot(h,xlab= "Production", ylab ="Months",main="Total Production", names.arg=m,border="black",col=c('green','yellow','white','blue','pink'))
+
