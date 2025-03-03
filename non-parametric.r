c9827feab5ff28fb8f9b2b5bc8d0d8143165ffec
Abu-Bakkar Siddik Zilhaj <160333632+Albinzer@users.noreply.github.com>
2025-03-03 21:44:09 +0600

    Update non-parametric.r

--- non-parametric.r
+++ non-parametric.r
@@ -1,27 +1,26 @@
-# Lab 19: Conduct Non-Parametric Tests
+# Define the two groups
+group1 <- c(85, 78, 92, 88, 76)
+group2 <- c(80, 82, 91, 77, 89, 93, 86)
 
-# Generate sample data
-set.seed(123)
-group1 <- rnorm(20, mean = 5, sd = 2)
-group2 <- rnorm(20, mean = 7, sd = 2)
-
-# Perform Wilcoxon rank-sum test (Mann-Whitney U test)
+# Perform the Wilcoxon Rank-Sum Test
 wilcox_test_result <- wilcox.test(group1, group2, exact = FALSE)
 
-# Output
-cat("Wilcoxon Rank-Sum Test Result:\n")
+# Print the results
 print(wilcox_test_result)
 
-# Graphical Output
-par(mfrow = c(1, 2))
+# Graphical Output: Boxplot to Compare Groups
+par(mfrow = c(1, 2))  # Set the plot area to 1 row and 2 columns
 
-# Boxplot of groups
+# Boxplot to compare the two groups
 boxplot(group1, group2, names = c("Group 1", "Group 2"), 
-        col = c("lightblue", "lightgreen"), main = "Boxplot of Groups", 
-        xlab = "Group", ylab = "Value")
+        col = c("lightblue", "lightgreen"),
+        main = "Boxplot of Group 1 and Group 2", 
+        ylab = "Values")
 
-# Density plots of groups
-plot(density(group1), col = "blue", lwd = 2, main = "Density Plots", 
-     xlab = "Value", ylim = c(0, 0.25))
+# Add a legend
+legend("topright", legend = c("Group 1", "Group 2"), fill = c("lightblue", "lightgreen"))
+
+# Density plot to compare the distributions of the groups
+plot(density(group1), col = "blue", lwd = 2, main = "Density Plot of Groups", xlab = "Values", ylim = c(0, 0.05))
 lines(density(group2), col = "green", lwd = 2)
 legend("topright", legend = c("Group 1", "Group 2"), col = c("blue", "green"), lwd = 2)
